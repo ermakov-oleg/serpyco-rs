45e0534f88cd5e6ef688f8158afe25fa7e315698
ermakov-oleg <ermakov-oleg@users.noreply.github.com>
2022-10-22 20:55:56 +0400

    Fix types

--- python/serpyco_rs/_utils.py
+++ python/serpyco_rs/_utils.py
@@ -3,6 +3,5 @@ import re
 CAMELCASE_RE = re.compile(r"(?!^)_([a-zA-Z])")
 
 
-def to_camelcase(s):
-    # type: (str) -> str
+def to_camelcase(s: str) -> str:
     return CAMELCASE_RE.sub(lambda m: m.group(1).upper(), s).rstrip("_")
